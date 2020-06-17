07cbcd3773ae7097998694e6adc24a3f3c8be4b3
HavrylivDenys <denys.havryliv@gmail.com>
2020-06-17 18:00:00 +0300

    Updates

---
+++ precache-manifest.94c0050b60e705c610016101dd31e7c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6816805c33e7edb164f7b2359daaa583",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "ddc4311b198d9f0279fd",
+    "url": "/monster-rolodex/static/css/main.d31a89bf.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddc4311b198d9f0279fd",
+    "url": "/monster-rolodex/static/js/main.d0141e2a.chunk.js"
+  },
+  {
+    "revision": "8575afebb5261fef1571",
+    "url": "/monster-rolodex/static/js/runtime-main.502e2e68.js"
+  }
+]);
